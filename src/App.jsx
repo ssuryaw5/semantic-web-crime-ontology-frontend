f6ae0788cdb5ccb9688d6b2604c10093d2a1f79b
Sumeet Laxman Suryawanshi <ssuryaw5@asu.edu>
2024-12-04 00:29:04 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div>"Welcome to semantic web ontology frontend"</div>;
+}
+
+export default App;
